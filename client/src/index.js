8b1e280c3608c7d41d7d01559deea72736d82132
kaetee425 <kingpang@outlook.com>
2018-03-25 16:02:20 -0700

    redux added

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux'
+import {Provider} from 'react-redux'
+import promise from 'redux-promise'
+import reduxThunk from 'redux-thunk'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware(promise, reduxThunk));
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>
+	, document.getElementById('root'));
