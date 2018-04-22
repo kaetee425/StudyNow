00b72006b679dc7245842e2264dea136008b2445
kaetee425 <kingpang@outlook.com>
2018-04-21 17:22:01 -0700

    Remove yelp-fusion

--- client/src/Pages/Locations.js
+++ client/src/Pages/Locations.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import Footer from '../Footer/Footer'
-import yelp from 'yelp-fusion';
-import API from '../utils/API'
-import yelplogo from './yelp.png'
 import {connect} from 'react-redux'
 import { fetchBizReview, fetchBiz } from './../actions'
 
@@ -10,122 +7,6 @@ import './Locations.css'
 import Navbar from '../Navbar/Navbar'
 
 class  Locations extends Component {
-
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			businesses: [],
-			reviews: [],
-			term: '',
-			location: ''
-		}
-
-		this.findOnChange = this.findOnChange.bind(this)
-		this.locationOnChange = this.locationOnChange.bind(this)
-
-	}
-
-	findOnChange = (event) => {
-		console.log(event.target.value)
-
-		this.setState({
-			term: event.target.value
-		})
-	}
-
-	componentWillReceiveProps = (newProps) => {
-		console.log('newProps', newProps);
-		if (newProps.biz) {
-			this.setState({
-				reviews: newProps.biz,
-				businesses: newProps.biz
-			});
-		}
-	}
-
-	locationOnChange = (event) => {
-		this.setState({
-			location: event.target.value
-		})
-	}
-
-	handleFormSubmit = (event) => {
-		event.preventDefault()
-
-		console.log(event)
-
-		// this.props.biz;
-		this.props.fetchBiz({
-			term: this.state.term,
-			location: this.state.location
-		})
-	}
-
-	handleShowReview = (bizID) => {
-		// event.preventDefault()
-		
-		this.props.fetchBizReview(bizID);
-		this.props.biz;
-		//this.props.review
-		// console.log(bizID)
-		console.log(this.props.biz)
-
-	}
-
-	renderReviewContent() {
-		const {reviews} = this.state
-
-		if (!reviews) {
-			<div>Be the first to write a review!</div>
-		}
-
-		return (
-			reviews.map(review => {
-				return(
-					<div key={review.id} className="reviews">
-						<p>{review.text}</p>
-					</div>	
-				)
-			})
-		)
-	}
-
-	renderContent() {
-		const {businesses} = this.state
-
-		if (!businesses) {
-			<div>loading...</div>
-		}
-
-		return (
-		businesses.map(business => {
-			console.log(business)
-			return(
-				<div key={business.id} className="container foodlist">
-					<div className="row">
-
-						<div className="col-md-4 foodimg">
-							<img src="http://lorempixel.com/output/nature-q-c-640-480-4.jpg" width="200px" height="200px" />
-							<button onClick={()=> this.handleShowReview(business.id)}>Click for reviews</button>
-						</div>
-
-						<div className="col-md-8 details">
-							<div>
-								<p className="bizname">{business.name}</p>
-								<p className="bizaddy">{business.location.display_address}</p>
-								<p className="bizphone">{business.display_phone}</p>
-								<p className="bizprice">{business.price}</p>
-								<div className="review">{this.renderReviewContent()}</div>
-								<a href="{business.url}"><img className="yelplogo" src={yelplogo} /></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			)
-		})
-		)
-	}
 
 	render() {
 
@@ -138,10 +19,6 @@ class  Locations extends Component {
 					<input name="location" id="cafes" onChange={this.locationOnChange} type="text" placeholder="Location: Current, SF, Redwood City..." />
 					<button onClick={this.handleFormSubmit}>Submit</button>
 				</form>
-
-				<div className='load'>
-					{this.renderContent()}
-				</div>
 
 				<Footer />
 			</div>
