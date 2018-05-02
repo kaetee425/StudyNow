f25ae2c80397510c1d97bf9ff8ad517d9accdae9
kaetee425 <kingpang@outlook.com>
2018-05-01 19:13:18 -0700

    Quick updates

--- routes/bizRoutes.js
+++ routes/bizRoutes.js
@@ -5,37 +5,43 @@ const Yelp = require('yelp')
 
 module.exports = app => {
 
-	// app.get('/api/business', (req, res) => {
-	// 	console.log("this is running")
-	// 	// axios.get("https://api.yelp.com/v3/businesses/search")
-	// 	// 	.then(response => {
-	// 	// 		console.log("yelp search: ", response)
-	// 	// 	}).catch(err => console.error(err))
+	app.get('/api/business', (req, res) => {
+		console.log("this is running")
+		// axios.get("https://api.yelp.com/v3/businesses/search")
+		// 	.then(response => {
+		// 		console.log("yelp search: ", response)
+		// 	}).catch(err => console.error(err))
 	
-	// 	const url = "https://api.yelp.com/v3/businesses/search"
+		const url = "https://api.yelp.com/v3/businesses/search"
 
-	// 	const options = {
-	// 		method: 'GET',
-	// 		headers: {app_id: keys.yelpClientID, app_secret: keys.yelpClientSecrete},
-	// 		url: url
-	// 	};
+		const options = {
+			method: 'GET',
+			headers: {app_id: keys.yelpClientID, app_secret: keys.yelpClientSecrete},
+			url: url
+		};
 
-	// 	axios(options)
-
-	// })
-
-	const yelp = new Yelp({
-		consumer_key: keys.yelpClientID,
-		consumer_secret: keys.yelpClientSecrete,
-	});
-
-	yelp.search({ term: 'food', location: 'Montreal' })
-		.then(function (data) {
-			console.log(data)
-		}).catch(function (err) {
-			console.errer(err)
-		})
+		axios(options)
+	})
 
 }
 
 //Authentication: Bearer <API_KEY> <-- from yelp itself
+
+// method: 'GET',
+//     url: 'yelpURL',
+//     headers: {
+//         'Authorization': 'Basic Y2xpZW50OnNlY3JldA==',
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+
+// var config = {
+  // headers: {Authorization: <KEY>}
+// };
+// axios.get(url, config) 
+
+// {
+// 	"access_token": "iSl",
+// 	"expires_in": 640100460,
+// 	"token_type": "Bearer"
+// }
+
